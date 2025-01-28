247eee7e81d266ecae0efcbbf7c7cb4ba6c29e53
israelnp <israelpereira1984@gmail.com>
2025-01-28 18:04:24 -0300

    fix container

--- vars/kanikoBuildPush.groovy
+++ vars/kanikoBuildPush.groovy
@@ -6,38 +6,19 @@ def call(body) {
 
   container('kaniko') {
     sh '''
-      set -e
 
       REGISTRY="docker.io"
       REPOSITORY="israelxnp/${JOB_NAME%/*}"
-      ENVIRONMENT=""
-      TAG=""
+      TAG="0.0.2"
 
-      if [[ "$GIT_BRANCH" == "develop" ]]; then
-        TAG="dev-${GIT_COMMIT:0:10}"
-        ENVIRONMENT="dev"
-      elif [[ "$GIT_BRANCH" =~ ^hotfix-.* ]]; then
-        TAG="${GIT_BRANCH#*-}-${GIT_COMMIT:0:10}"
-        ENVIRONMENT="stg"
-      else
-        echo "Erro: Branch ${GIT_BRANCH} não suportado."
-        exit 1
-      fi
 
       DESTINATION="${REGISTRY}/${REPOSITORY}:${TAG}"
 
-      if [[ ! -f Dockerfile ]]; then
-        echo "Erro: Dockerfile não encontrado."
-        exit 1
-      fi
-
       /kaniko/executor \
+        --insecure \
         --destination "${DESTINATION}" \
         --context $(pwd) \
-        --dockerfile $(pwd)/Dockerfile
 
-      mkdir -p /artifacts
-      echo "${TAG}" > /artifacts/${ENVIRONMENT}.artifact
     '''
   }
 }
