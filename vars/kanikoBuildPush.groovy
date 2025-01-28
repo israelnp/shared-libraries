1defd56cbcff4a80dd681aac8c08002374e1efab
israelnp <israelpereira1984@gmail.com>
2025-01-28 15:10:46 -0300

    fix kaniko function 19

--- vars/kanikoBuildPush.groovy
+++ vars/kanikoBuildPush.groovy
@@ -1,5 +1,4 @@
-def call (body) {
-
+def call(body) {
   def settings = [:]
   body.resolveStrategy = Closure.DELEGATE_FIRST
   body.delegate = settings
@@ -7,31 +6,36 @@ def call (body) {
 
   container('kaniko') {
     sh '''
+      set -e
+
       REGISTRY="docker.io"
       REPOSITORY="israelxnp/${JOB_NAME%/*}"
       ENVIRONMENT=""
       TAG=""
 
-      if [ $(echo $GIT_BRANCH | grep ^develop$) ]; then
+      # Verificação do branch e definição de TAG e ENVIRONMENT
+      if [[ "$GIT_BRANCH" == "develop" ]]; then
         TAG="dev-${GIT_COMMIT:0:10}"
         ENVIRONMENT="dev"
-      elif [ $(echo $GIT_BRANCH | grep -E "^hotfix-.*") ]; then
+      elif [[ "$GIT_BRANCH" =~ ^hotfix-.* ]]; then
         TAG="${GIT_BRANCH#*-}-${GIT_COMMIT:0:10}"
         ENVIRONMENT="stg"
+      else
+        echo "Branch não suportado: $GIT_BRANCH"
+        exit 1
       fi
 
       DESTINATION="${REGISTRY}/${REPOSITORY}:${TAG}"
 
       # Executar Kaniko para build e push da imagem
-        /kaniko/executor \
-          --destination "${DESTINATION}" \
-          --context $(pwd) \
-          --dockerfile $(pwd)/Dockerfile
+      /kaniko/executor \
+        --destination "${DESTINATION}" \
+        --context $(pwd) \
+        --dockerfile $(pwd)/Dockerfile
 
-        # Salvar TAG no arquivo de artefato
-        mkdir -p /artifacts
-        echo "${TAG}" > /artifacts/${ENVIRONMENT}.artifact
+      # Salvar TAG no arquivo de artefato
+      mkdir -p /artifacts
+      echo "${TAG}" > /artifacts/${ENVIRONMENT}.artifact
     '''
   }
-
 }
