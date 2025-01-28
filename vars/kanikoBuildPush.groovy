df197d5a60270edcae35f64cfb3a56a5830ef093
israelnp <israelpereira1984@gmail.com>
2025-01-28 14:24:59 -0300

    fix kaniko function 17

--- vars/kanikoBuildPush.groovy
+++ vars/kanikoBuildPush.groovy
@@ -5,39 +5,41 @@ def call(body) {
   body()
 
   container('kaniko') {
-  withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
-    sh '''
-      set -e
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
+      sh '''
+        set -e
 
-      # Realizar login no Docker Hub
-      echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin docker.io
+        # Configurações do Docker Registry
+        REGISTRY="docker.io"
+        REPOSITORY="israelxnp/${JOB_NAME%/*}"
+        ENVIRONMENT=""
+        TAG=""
 
-      # Definir TAG e ENVIRONMENT com base no branch
-      REGISTRY="docker.io"
-      REPOSITORY="israelxnp/${JOB_NAME%/*}"
-      ENVIRONMENT=""
-      TAG=""
+        # Definir TAG e ENVIRONMENT com base no branch
+        if [[ "$GIT_BRANCH" == "develop" ]]; then
+          TAG="dev-${GIT_COMMIT:0:10}"
+          ENVIRONMENT="dev"
+        elif [[ "$GIT_BRANCH" == hotfix-* ]]; then
+          TAG="${GIT_BRANCH#hotfix-}-${GIT_COMMIT:0:10}"
+          ENVIRONMENT="stg"
+        else
+          echo "Branch não suportado: $GIT_BRANCH"
+          exit 1
+        fi
 
-      if [[ "$GIT_BRANCH" == "develop" ]]; then
-        TAG="dev-${GIT_COMMIT:0:10}"
-        ENVIRONMENT="dev"
-      elif [[ "$GIT_BRANCH" == hotfix-* ]]; then
-        TAG="${GIT_BRANCH#hotfix-}-${GIT_COMMIT:0:10}"
-        ENVIRONMENT="stg"
-      else
-        echo "Branch não suportado: $GIT_BRANCH"
-        exit 1
-      fi
+        # Destino da imagem
+        DESTINATION="${REGISTRY}/${REPOSITORY}:${TAG}"
 
-      DESTINATION="${REGISTRY}/${REPOSITORY}:${TAG}"
+        # Executar Kaniko para build e push da imagem
+        /kaniko/executor \
+          --destination "${DESTINATION}" \
+          --context $(pwd) \
+          --dockerfile $(pwd)/Dockerfile
 
-      # Executar Kaniko para build e push da imagem
-      /kaniko/executor --destination "${DESTINATION}" --context $(pwd)
-
-      # Salvar TAG no arquivo de artefato
-      mkdir -p /artifacts
-      echo "${TAG}" > /artifacts/${ENVIRONMENT}.artifact
-    '''
+        # Salvar TAG no arquivo de artefato
+        mkdir -p /artifacts
+        echo "${TAG}" > /artifacts/${ENVIRONMENT}.artifact
+      '''
+    }
   }
-}
 }
