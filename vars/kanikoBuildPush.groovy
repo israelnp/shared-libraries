05c2ca53d556fefa6467eab528bc51071059fd9b
israelnp <israelpereira1984@gmail.com>
2025-01-29 09:34:21 -0300

    new format2

--- vars/kanikoBuildPush.groovy
+++ vars/kanikoBuildPush.groovy
@@ -6,14 +6,27 @@ def call(body) {
 
   container('kaniko') {
     sh '''
+        REGISTRY="docker.io/israelxnp"
+              REPOSITORY=${JOB_NAME%/*}
+              TAG=""
+              ENVIRONMENT=""
 
+              if [ $(echo $GIT_BRANCH | grep ^develop$) ]; then
+                TAG="dev-${GIT_COMMIT:0:10}"
+                ENVIRONMENT="dev"
+              elif [ $(echo $GIT_BRANCH | grep -E "^hotfix-.*") ]; then
+                TAG="${GIT_BRANCH#*-}-${GIT_COMMIT:0:10}"
+                ENVIRONMENT="stg"
+              fi
 
-      cat /kaniko/.docker/config.json
-      
-     /kaniko/executor \
-          --dockerfile=/workspace/Dockerfile \
-          --context=dir:///workspace \
-          --destination=docker.io/israelxnp/teste3:0.0.2 \
+              DESTINATION="${REGISTRY}/${REPOSITORY}:${TAG}"
+
+              /kaniko/executor \
+                --insecure \
+                --destination "${DESTINATION}" \
+                --context $(pwd)
+
     '''
+
   }
 }
