fc502af03fd0a20ba02aa67ab95c829ed8518439
israelnp <israelpereira1984@gmail.com>
2025-02-02 11:30:43 -0300

    fix config

--- vars/kanikoBuildPush.groovy
+++ vars/kanikoBuildPush.groovy
@@ -4,29 +4,13 @@ def call(body) {
   body.delegate = settings
   body()
 
-  container('kaniko') {
-    sh '''
-        REGISTRY="docker.io/israelxnp"
-              REPOSITORY=${JOB_NAME%/*}
-              TAG=""
-              ENVIRONMENT=""
-
-              if [ $(echo $GIT_BRANCH | grep ^develop$) ]; then
-                TAG="dev-${GIT_COMMIT:0:10}"
-                ENVIRONMENT="dev"
-              elif [ $(echo $GIT_BRANCH | grep -E "^hotfix-.*") ]; then
-                TAG="${GIT_BRANCH#*-}-${GIT_COMMIT:0:10}"
-                ENVIRONMENT="stg"
-              fi
-
-              DESTINATION="${REGISTRY}/${REPOSITORY}:${TAG}"
-
-              /kaniko/executor \
-                --insecure \
-                --destination "${DESTINATION}" \
-                --context $(pwd)
-
-    '''
-
+  container('buildah') {
+    withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]) {
+      sh """
+        echo $DOCKER_PASSWORD | buildah login -u $DOCKER_USERNAME --password-stdin docker.io
+        buildah bud -f /workspace/Dockerfile -t docker.io/israelxnp/teste3:0.0.2 /workspace
+        buildah push docker.io/israelxnp/teste3:0.0.2
+      """
+    }
   }
 }
