f61da6dd9b074b5be1d7f361c9addeb4bce09a2a
israelnp <israelpereira1984@gmail.com>
2025-02-02 12:00:55 -0300

    fix config2

--- vars/kanikoBuildPush.groovy
+++ vars/kanikoBuildPush.groovy
@@ -7,8 +7,9 @@ def call(body) {
   container('buildah') {
     withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]) {
       sh """
+        ls -la
         echo $DOCKER_PASSWORD | buildah login -u $DOCKER_USERNAME --password-stdin docker.io
-        buildah bud -f /workspace/Dockerfile -t docker.io/israelxnp/teste3:0.0.2 /workspace
+        buildah bud -f /workspace/Dockerfile -t docker.io/israelxnp/teste3:0.0.2 $(pwd)
         buildah push docker.io/israelxnp/teste3:0.0.2
       """
     }
