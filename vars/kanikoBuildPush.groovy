935f6ef54d7037037fc3b7f59b8849aa2a7919f4
israelnp <israelpereira1984@gmail.com>
2025-02-02 13:10:00 -0300

    fix config7

--- vars/kanikoBuildPush.groovy
+++ vars/kanikoBuildPush.groovy
@@ -6,29 +6,29 @@ def call(body) {
 
   container('buildah') {
     withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]) {
-      sh """
+      sh '''
         REGISTRY="docker.io/israelxnp"
-        REPOSITORY=\$(echo "$JOB_NAME" | awk -F'/' '{print \$1}')
+        REPOSITORY=$(echo "$JOB_NAME" | awk -F'/' '{print $1}')
         TAG=""
         ENVIRONMENT=""
 
-        if echo "\$GIT_BRANCH" | grep -q '^develop$'; then
-          TAG="dev-\$(echo \$GIT_COMMIT | cut -c1-10)"
+        if echo "$GIT_BRANCH" | grep -q "^develop$"; then
+          TAG="dev-$(echo $GIT_COMMIT | cut -c1-10)"
           ENVIRONMENT="dev"
-        elif echo "\$GIT_BRANCH" | grep -E -q "^hotfix-.*"; then
-          TAG="\$(echo "\$GIT_BRANCH" | cut -d'-' -f2)-\$(echo \$GIT_COMMIT | cut -c1-10)"
+        elif echo "$GIT_BRANCH" | grep -E -q "^hotfix-.*"; then
+          TAG="$(echo "$GIT_BRANCH" | cut -d'-' -f2)-$(echo $GIT_COMMIT | cut -c1-10)"
           ENVIRONMENT="stg"
         else
           TAG="latest"
           ENVIRONMENT="prod"
         fi
 
-        DESTINATION="\${REGISTRY}/\${REPOSITORY}:\${TAG}"
+        DESTINATION="${REGISTRY}/${REPOSITORY}:${TAG}"
 
-        echo "\$DOCKER_PASSWORD" | buildah login -u "\$DOCKER_USERNAME" --password-stdin docker.io
-        buildah bud -f Dockerfile -t "\$DESTINATION" "\$(pwd)"
-        buildah push "\$DESTINATION"
-      """
+        echo "$DOCKER_PASSWORD" | buildah login -u "$DOCKER_USERNAME" --password-stdin docker.io
+        buildah bud -f Dockerfile -t "$DESTINATION" "$(pwd)"
+        buildah push "$DESTINATION"
+      '''
     }
   }
 }
