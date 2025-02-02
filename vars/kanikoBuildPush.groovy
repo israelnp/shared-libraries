d3261e1aa2ff651cf7112238dab4acff099248f7
israelnp <israelpereira1984@gmail.com>
2025-02-02 12:58:38 -0300

    fix config5

--- vars/kanikoBuildPush.groovy
+++ vars/kanikoBuildPush.groovy
@@ -7,10 +7,24 @@ def call(body) {
   container('buildah') {
     withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]) {
       sh """
-        ls -la
+        REGISTRY="docker.io/israelxnp"
+        REPOSITORY=${JOB_NAME%/*}
+        TAG=""
+        ENVIRONMENT=""
+
+        if [ $(echo $GIT_BRANCH | grep ^develop$) ]; then
+          TAG="dev-${GIT_COMMIT:0:10}"
+          ENVIRONMENT="dev"
+        elif [ $(echo $GIT_BRANCH | grep -E "^hotfix-.*") ]; then
+          TAG="${GIT_BRANCH#*-}-${GIT_COMMIT:0:10}"
+          ENVIRONMENT="stg"
+        fi
+
+        DESTINATION="${REGISTRY}/${REPOSITORY}:${TAG}"
+
         echo $DOCKER_PASSWORD | buildah login -u $DOCKER_USERNAME --password-stdin docker.io
-        buildah bud -f Dockerfile -t docker.io/israelxnp/teste3:0.0.2 \$(pwd)
-        buildah push docker.io/israelxnp/teste3:0.0.2
+        buildah bud -f Dockerfile -t $DESTINATION \$(pwd)
+        buildah push $DESTINATION 
       """
     }
   }
